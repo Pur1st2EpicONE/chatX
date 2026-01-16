5b507e75903c346aa2885db78bd514cd85d63526
Pur1st2EpicONE <vizor20000@mail.ru>
2026-01-16 18:26:00 +0200

    update schema

--- migrations/000001_init.sql
+++ migrations/000001_init.sql
@@ -1,11 +1,11 @@
 -- +goose Up
-CREATE TABLE chats (
+CREATE TABLE IF NOT EXISTS chats (
     id          INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title       TEXT NOT NULL,
     created_at  TIMESTAMPTZ NOT NULL
 );
 
-CREATE TABLE messages (
+CREATE TABLE IF NOT EXISTS messages (
     id          INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     chat_id     INTEGER NOT NULL,
     text        TEXT NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE messages (
     CONSTRAINT  fk_messages_chat FOREIGN KEY (chat_id) REFERENCES chats(id) ON DELETE CASCADE
 );
 
-CREATE INDEX idx_messages_chat_id_created_at ON messages(chat_id, created_at DESC);
+CREATE INDEX IF NOT EXISTS idx_messages_chat_id_created_at ON messages(chat_id, created_at DESC);
 
 -- +goose Down
 DROP TABLE IF EXISTS messages CASCADE;
